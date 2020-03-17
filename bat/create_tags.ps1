5968152039d457901a4d369e5f779ab3583dba05
ohtorii <ikeuchi.h9@gmail.com>
2020-03-17 21:24:28 +0900

    Create create_tags.ps1

---
+++ bat/create_tags.ps1
@@ -0,0 +1,34 @@
+﻿$confPath="tags.json";
+
+foreach($item in (Get-Content $confPath | ConvertFrom-Json)){
+	$command = "/c pushd `"{0}`"&&ctags {1}" -f $item.path, $item.ctags_options;	
+	Write-Host $command;	
+	Start-Process -FilePath "cmd" -ArgumentList $command;
+}
+
+
+
+# komwnkomennto 
+<# komennto 
+コメントです
+# >
+#>
+<#
+foreach ($line in Get-Content tags.conf) {
+	if($line.trim() -eq ""){
+		#空白行なので無視する
+		continue;
+	}
+
+	if($line.StartsWith("#")){
+		#コメント行なので無視する
+		continue;
+	}
+	
+	pusd $line
+	ctagを生成する
+	popd
+	
+	Write-Host $line;
+}
+#>
