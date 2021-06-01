16abcb19840e7e943837aaa586d4e6484f12a57d
ohtorii <ikeuchi.h9@gmail.com>
2021-06-01 16:20:43 +0900

    Update

---
+++ bat/bin/display_off.ps1
@@ -0,0 +1,13 @@
+﻿$methods = @'
+[return: MarshalAs(UnmanagedType.Bool)]
+[DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+public static extern bool PostMessage(uint hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+[DllImport("user32.dll", SetLastError = true)]
+public static extern bool LockWorkStation();
+'@
+
+$pwm = Add-Type -MemberDefinition $methods -Name "PowerManager" -PassThru -Language CSharp
+
+$pwm::PostMessage(0xffff, 0x0112, 0xf170, 2)
+# $pwm::LockWorkStation()
---
+++ bat/display_off.bat
@@ -0,0 +1,2 @@
+@echo off
+powershell -NoProfile -ExecutionPolicy Unrestricted %~dp0\bin\display_off.ps1
