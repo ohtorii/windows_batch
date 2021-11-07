f512559a7553a47160a16462d91604810563cbe9
ohtorii <ikeuchi.h9@gmail.com>
2021-11-07 17:27:44 +0900

    Update dumpbin_depend.bat

--- bat/dumpbin_depend.bat
+++ bat/dumpbin_depend.bat
@@ -1,6 +1,14 @@
 @echo off
+setlocal
 REM 
 REM 実行ファイルの依存関係を表示する
 REM 
-"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\dumpbin.exe" /dependents %1
+call %~dp0bin\FindVsDevCmd.bat
+if "%VSDEVCMD%"=="" (
+    echo VisualStudio開発者用コマンドプロンプトのバッチファイルが見つかりません。
+    pause
+    exit /b 1
+)
+call "%VSDEVCMD%"
+dumpbin.exe /dependents %1
 pause
