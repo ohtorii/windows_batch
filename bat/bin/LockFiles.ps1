7beead1d038defe6d466d1ad75386e79806753a0
ohtorii <ikeuchi.h9@gmail.com>
2021-11-19 19:32:20 +0900

    Create LockFiles.ps1

---
+++ bat/bin/LockFiles.ps1
@@ -0,0 +1,24 @@
+﻿foreach ($arg in $args) {
+    if (-not(Test-Path $arg)){
+        Write-Host 引数で指定されたファイルが存在しません
+        Write-Host $arg
+        exit 
+    }
+}
+
+Write-Host "ファイルをロック中"
+$files = @()
+foreach ($arg in $args) {
+    $file = [System.IO.File]::Open($arg,[System.IO.FileMode]::Open,[System.IO.FileAccess]::Read,[System.IO.FileShare]::None)
+    $files += $file
+    Write-Host [OK] $arg
+}
+
+Write-Host ""
+Read-Host "続けるには Enter キーを押してください..." 
+
+# UnLock
+foreach($file in $files){
+    $file.Close()
+}
+
