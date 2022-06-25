7e36fe19c7c5aa3d1dbd15d62043241d540bad86
ohtorii <ikeuchi.h9@gmail.com>
2022-06-25 22:15:17 +0900

    Update zip-drop-files.bat

--- bat/zip-drop-files.bat
+++ bat/zip-drop-files.bat
@@ -1,6 +1,6 @@
 @echo off
 REM 
-REM dropしたファイルル、フォルダをzipに固める
+REM dropしたファイル、フォルダをzipに固める
 REM 
 
 setlocal enabledelayedexpansion
@@ -10,9 +10,9 @@ pause
 exit /b 0
 
 :Main
-	call :GetDirName %1
+	call :GetDirName "%~1"
 	set PARENT_DIR=%~dp1
-	7z.exe a "!PARENT_DIR!\!CDN!.zip" %1
+	7z.exe a "!PARENT_DIR!\!CDN!.zip" "%~1"
 	exit /b 0
 
 :GetDirName
@@ -20,7 +20,7 @@ exit /b 0
 	REM %1   =C:\Document and Setting/foo/workspace
 	REM %CDN%=workspace
 	REM 
-	set CDN=%1
+	set CDN="%~1"
 	set CDN=%CDN:"=%
 	set CDN=%CDN:/=\%
 	if "%CDN:~-1%"=="\" set CDN=%CDN:~0,-1%
